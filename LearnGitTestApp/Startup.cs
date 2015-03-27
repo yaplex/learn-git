fffe5543aab14949e1f0301ece4bde3bc8f7a593
Alex Shapovalov <alex@yaplex.com>
2015-03-27 16:08:17 -0400

    test app

---
+++ LearnGitTestApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LearnGitTestApp.Startup))]
+namespace LearnGitTestApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
